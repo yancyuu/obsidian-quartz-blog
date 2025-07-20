ddc372038e0f3183c318aa2875bb792958e4f351
yancyyu <yancyyu.ok@gmail.com>
2025-07-20 13:08:43 +0800

    update config

--- source/quartz/components/scripts/darkmode.inline.ts
+++ source/quartz/components/scripts/darkmode.inline.ts
@@ -1,4 +1,4 @@
-const userPref = window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark"
+const userPref = window.matchMedia("(prefers-color-scheme: dark)").matches ? "light" : "dark"
 const currentTheme = localStorage.getItem("theme") ?? userPref
 document.documentElement.setAttribute("saved-theme", currentTheme)
 
